4f594253af84be3423dd38195460ff74d2804220
Leone Odinga <leone.odinga@cloudproductivity-solutions.com>
2024-06-19 16:25:10 +0300

    modified the read state file functinality

--- forward_oracle_logs.py
+++ forward_oracle_logs.py
@@ -44,7 +44,7 @@ def create_table_file():
             pass
 
 '''Read the current state of the file and store it in a dictionary'''
-def read_state(table_name):
+def read_state():
     state = {};
     if os.path.exists(STATE_FILE):
         with open(STATE_FILE, "r") as f:
@@ -90,14 +90,9 @@ def main():
     # Define a state dictionary to keep track of the last sent logs for each table
     state = {}
 
-    # Read the state file for each table
-    for table in oracle_database_tables:
-        if table != "":
-            state = read_state(table)
-            print(state)
-        else:
-            print("Cannot fetch the state of the speficied table. Table Missing!")
-
+    # Read the state for the tables
+    state = read_state()
+    
     # Prompt the user for the Oracle DB password
     ORACLE_PASSWORD = getpass.getpass("Enter Oracle DB Password: ")
 
