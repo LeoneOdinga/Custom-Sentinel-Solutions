3bd0683256b66da1f16cb7cb169c19adfdcf8af8
Leone Odinga <leone.odinga@cloudproductivity-solutions.com>
2024-06-19 11:00:39 +0300

    updating data_inputs file

--- data_inputs.py
+++ data_inputs.py
@@ -1,7 +1,8 @@
 #CONSTANTS
-SYSLOG_SVR_IP = "10.0.0.5" # define the syslog server IP address
-SYSLOG_SVR_PORT = 514    # define the syslog server port 
-STATE_FILE = '.state'    # this is the name of the state file that would keep track of the last sent logs to sentinel
+SYSLOG_SVR_IP = "10.0.0.5" #define the syslog server IP address
+SYSLOG_SVR_PORT = 514    #define the syslog server port 
+STATE_FILE = '.state'    #state file that keeps track of the last logs sent to sentinel
+TABLES_FILE = 'tables.txt' #file to insert the table names you want the script to target
 
 # ORACLE DATABASE DETAILS
 ORACLE_USERNAME = "script"
