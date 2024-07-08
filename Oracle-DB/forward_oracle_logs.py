e04f0464473407a2a6a50d677921af0a58edcabf
Leone Odinga <leone.odinga@cloudproductivity-solutions.com>
2024-07-08 16:15:47 +0300

    TR53

--- Oracle-DB/forward_oracle_logs.py
+++ Oracle-DB/forward_oracle_logs.py
@@ -167,6 +167,7 @@ def main():
                 # Define the SQL query and offset query results with last rows read
                 last_rows_read = state.get(table,0)
                 sql_query = f"SELECT " + ", ".join(columns_to_select) + " FROM {table}"
+
                 total_rows = execute_query(cursor,sql_query)
                 count_total_rows = len(total_rows)
 
